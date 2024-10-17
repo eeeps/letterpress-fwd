c06fa0d2c1a436fe8589585c0a99bc74af4e63d1
Eric Portis <e@ericportis.com>
2024-10-17 07:19:14 -0700

    Fancier no-JS form submission response
    More informative errors, HTML on success.
    Progress on #1

--- netlify/functions/handleSignup.js
+++ netlify/functions/handleSignup.js
@@ -3,7 +3,7 @@ export default async ( req, context ) => {
 	if ( req.method !== 'POST' ) {
 		return new Response('405 Method Not Allowed', {
 			status: 405,
-			statusText: 'Method Not Allowed'
+			statusText: 'We only accept POSTs'
 		} );
 	}
 	
@@ -11,7 +11,7 @@ export default async ( req, context ) => {
 	     !req.headers.get( 'content-type' ).toLowerCase() === 'application/x-www-form-urlencoded' ) {
 		return new Response('415 Unsupported Media Type', {
 			status: 415,
-			statusText: 'Unsupported Media Type'
+			statusText: 'Requests must have Content-Type: application/x-www-form-urlencoded'
 		} );
 	}
 	
@@ -22,10 +22,12 @@ export default async ( req, context ) => {
 	     !fd.has('name') ) {
 		return new Response('400 Bad Request', {
 			status: 400,
-			statusText: 'Bad Request'
+			statusText: 'Requests must include `email` and `name` keys'
 		} );
 	}
 
+	// todo? get rid of the automation and just make this a regular API request
+	// (hiding the Personal Access Token in a secret, of course)
 	const url = 'https://hooks.airtable.com/workflows/v1/genericWebhook/appffk9L1NpnhSONs/wflXrHkPvtFKNX91y/wtrk1IkFQDIAGgEx1'
 	const airtableResponse = await fetch( url, {
 	    method: 'POST',
@@ -33,16 +35,41 @@ export default async ( req, context ) => {
 	} );
 	
 	if ( airtableResponse.ok ) {
-		// todo pretty page for no JS folks?
-		return new Response( '200 OK', {
-			status: 200,
-			statusText: 'OK'
-		} )
+		// todo also check for success:true? in the body (if we keep doing the automation thing)
+		return new Response( `<!doctype html>
+<html lang="en">
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width" />
+<title>Thank you!</title>
+<link rel=stylesheet href="/style.css" />
+<style>
+html {
+	text-align: center;
+}
+</style>
+</head>
+<body>
+
+<h1>Thank you!</h1>
+<p>We’ll be in touch.</p>
+
+</body>
+</html>
+`,
+			{
+				status: 200,
+				statusText: 'OK',
+				headers: {
+					'content-type': 'text/html'
+				}
+			}
+		)
 	} else {
 		// todo pass errors
-		return new Response( '500 Internal Server Error', {
+		const body = await airtableResponse.text();
+		return new Response( `Upstream error; HTTP code ${ airtableResponse.status } ${ airtableResponse.statusText }; said, “${ body }”`, {
 			status: 500,
-			statusText: '500 Internal Service Error'
+			statusText: 'Internal Service Error'
 		} )
 	}
 
