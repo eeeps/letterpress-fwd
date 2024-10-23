5fc188fe053b4f5cbc5d0d9d6d2ffa7d5f480c1e
Eric Portis <e@ericportis.com>
2024-10-23 08:22:58 -0700

    Active slide dark, rest grayed out

--- scripts/looping-carousel.js
+++ scripts/looping-carousel.js
@@ -7,10 +7,40 @@ class LoopingCarousel extends HTMLElement {
 
 		super(); // duper
 	
+	
+		const threshold = 0.99 // weird misses when it's 1
+		const options = {
+			root: this,
+			rootMargin: "0px",
+			threshold: threshold 
+		};
+		
+		const callback = ( entries, observer ) => {
+			entries.forEach( entry => {
+				console.log( entry );
+				if ( entry.intersectionRatio >= threshold ) { // if element just became fully visible
+					entry.target.classList.add( 'visible' );
+				}
+				else { // otherwise it just became *not* fully visible
+					entry.target.classList.remove( 'visible' );
+				}
+				// css uses these classes to style...
+			} );
+		}
+
+		this.intersectionObserver = new IntersectionObserver( callback, options );
+	
 	}
 	
 	connectedCallback() {
-
+		
+		const carousel = [ ...this.children ][ 0 ];
+		const slides = [ ...carousel.children ];
+		
+		slides.forEach( slide => {
+			this.intersectionObserver.observe( slide )
+		} );
+		
 	}
 
 }
