838458f74d9a86b00facf74ca790890292854733
Eric Portis <e@ericportis.com>
2024-11-05 10:13:43 -0800

    Looping carousel progress

--- scripts/looping-carousel.js
+++ scripts/looping-carousel.js
@@ -15,9 +15,11 @@ class LoopingCarousel extends HTMLElement {
 			threshold: threshold 
 		};
 		
-		const callback = ( entries, observer ) => {
+		const io_callback = ( entries, observer ) => {
 			entries.forEach( entry => {
-				console.log( entry );
+				console.log( this.observedWidth );
+				console.log( this.slideWrapper.getBoundingClientRect() );
+				//console.log( entry );
 				if ( entry.intersectionRatio >= threshold ) { // if element just became fully visible
 					entry.target.classList.add( 'visible' );
 				}
@@ -28,18 +30,78 @@ class LoopingCarousel extends HTMLElement {
 			} );
 		}
 
-		this.intersectionObserver = new IntersectionObserver( callback, options );
+		this.intersectionObserver = new IntersectionObserver( io_callback, options );
+	
+		const ro_callback = ( entries => {
+			entries.forEach( entry => {
+				this.observedWidth = entry.borderBoxSize[ 0 ].inlineSize;
+// 				console.log( this.observedWidth );
+			} );
+		} );
+		
+		this.resizeObserver = new ResizeObserver( ro_callback );
 	
 	}
 	
-	connectedCallback() {
+	async connectedCallback() {
 		
 		const carousel = [ ...this.children ][ 0 ];
-		const slides = [ ...carousel.children ];
+		const originalSlides = [ ...carousel.children ];
 		
-		slides.forEach( slide => {
+		// for finding the one slide to highlight, with CSS		
+		const activeSlideWindow = document.createElement( 'div' );
+		activeSlideWindow.classList.add( 'activeSlideWindow' );
+		this.prepend( activeSlideWindow ); // sibling of the carousel
+		
+		// wrapper just for measuring
+		const slideWrapper = document.createElement( 'div' );
+		slideWrapper.classList.add( 'slideWrapper' );
+		this.slideWrapper = slideWrapper; //todo
+		carousel.prepend( slideWrapper );
+		originalSlides.forEach( slide => slideWrapper.appendChild( slide ) );
+		
+		// after all of the images have loaded, we can measure things
+		// i mean, hopefully we are reserving space, using <img height width>,
+		// and we could have gotten to work immediately
+		// but in case not...
+		const orignalSlidesImgs = [ ...carousel.querySelectorAll( 'img' ) ];
+		const imgLoadPromises = orignalSlidesImgs.map( img => 
+			( new Promise( (resolve, reject ) => {
+				if ( img.complete ) {
+// 					console.log( img );
+// 					console.log( 'was already complete' );
+					resolve( img );
+				} else {
+					img.addEventListener( 'load', () => {
+// 						console.log( img );
+// 						console.log( 'finished loading' );
+						resolve( img );
+					} );
+				}
+			} ) )
+		);
+		await Promise.all( imgLoadPromises );
+		// console.log('all of the images loaded');
+		
+// 		originalSlides.forEach()
+		this.resizeObserver.observe( slideWrapper, { box: 'border-box' } );
+		
+		const beforeSlides = originalSlides
+			.map( slide =>  slide.cloneNode( true ) );
+		const afterSlides = originalSlides
+			.map( slide =>  slide.cloneNode( true ) );
+		
+// 		beforeSlides.toReversed().forEach( slide =>
+// 			slideWrapper.prepend( slide )
+// 		);
+// 		afterSlides.forEach( slide => 
+// 			slideWrapper.append( slide )
+// 		);
+		
+		[ ...beforeSlides, ...originalSlides, ...afterSlides ].forEach( slide => {
 			this.intersectionObserver.observe( slide )
 		} );
+		originalSlides[0].scrollIntoView();
 		
 	}
 
